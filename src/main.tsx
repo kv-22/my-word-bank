a91970bc6d30d89b2584b6edfda14dfb186dd5c9
kv-22 <kavyadhawan22@yahoo.com>
2026-05-19 19:58:54 +0300

    fix reload

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import App from "./App.tsx";
 import "./index.css";
+
+registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    window.location.reload();
+  },
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
